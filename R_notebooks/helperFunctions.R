2aba2a53bcb33e5f3612136fb80703fccb9a8930
Alexander Cope <alexandercope@Alexanders-MacBook-Air-5.local>
2025-06-15 19:24:03 -0400

    Removing committed files that were removed without using git

--- R_notebooks/helperFunctions.R
+++ R_notebooks/helperFunctions.R
@@ -54,7 +54,9 @@ cleanSpeciesLabelsForPlots <- function(x)
 }
 
 
-calculateAcrossSpeciesCorrelations <- function(df,tree,x.var,y.var,...) 
+
+
+calculateAcrossSpeciesCorrelations <- function(df,tree,x.var,y.var,approach="standard",...) 
 {
     x.var.sym <- as.name(x.var)
     y.var.sym <- as.name(y.var)
@@ -73,13 +75,126 @@ calculateAcrossSpeciesCorrelations <- function(df,tree,x.var,y.var,...)
         tmp.tree <- cleaned$phy
         x.values <- x[,x.var]
         y.values <- x[,y.var]
-        x.pic <- calculatePIC(x.values[tmp.tree$tip.label],tree = tmp.tree)
-        y.pic <- calculatePIC(y.values[tmp.tree$tip.label],tree=tmp.tree)
-        df <- data.frame(X = x.pic[,1],Y = y.pic[,1])
-        return(cor.test(df$X,df$Y,...) %>% broom::tidy())
+        x.values <- x.values[tmp.tree$tip.label]
+        y.values <- y.values[tmp.tree$tip.label]
+        if (approach == "standard")
+        {
+          x.pic <- calculatePIC(x.values,tree = tmp.tree)
+          y.pic <- calculatePIC(y.values,tree=tmp.tree)
+          df <- data.frame(X = x.pic[,1],Y = y.pic[,1])
+          return(cor.test(df$X,df$Y,...) %>% broom::tidy())
+        } else {
+          result <- picRegression(tmp.tree,x.values,y.values,method="rank", sigTest="permutation",nperm=9999)
+          df <- data.frame(estimate=result$testStat,p.value=result$pVal)
+          return(df)
+        }
     }, error=function(e){return(NA)})
     }
 }
+
+picRegression <- function (tree, x, y, method="standard", sigTest="permutation",
+                           ...) 
+{
+  ## original version was missing nperm argument so I added it (LR)
+  if(hasArg(nperm)) nperm<-list(...)$nperm
+  else nperm<-100
+  if (!inherits(tree, "phylo")) 
+    stop("tree should be object of class \"phylo\".")
+  
+  if (method %in% c("standard", "sign", "rank") == FALSE) {
+    cat("  Invalid model. Setting model=\"standard\"\n\n")
+    model <- "standard"
+  }
+  
+  if (sigTest %in% c("analytic", "permutation") == FALSE) {
+    cat("  Invalid model. Setting model=\"permutation\"\n\n")
+    sigTest <- "permutation"
+  }
+  
+  if((method=="sign" | method=="rank")&sigTest=="analytic") {
+    cat("  No analytic p-value method exists for method ", method, ". Setting model=\"permutation\"\n\n")
+    sigTest <- "permutation"
+  }
+  
+  if(method=="standard") 
+  {
+    icx<-ape::pic(x, tree)
+    icy<-ape::pic(y, tree)
+    
+    if(sigTest=="analytic") 
+    {
+      res<-summary(lm(icy~icx+0))
+      testStat<-res$coefficients[1,3]
+      pVal<-res$coefficients[1,4]
+    }
+    if(sigTest=="permutation") 
+    {
+      dicx<-c(icx, -icx)	
+      dicy<-c(icy, -icy)
+      res<-summary(lm(dicy~dicx))
+      testStat<-res$coefficients[2,3]
+      nullDist<-numeric(nperm)
+      for(i in 1:nperm) 
+      {
+        picx<-sample(icx)
+        dpicx<-c(picx, -picx)
+        res<-summary(lm(dicy~dpicx))
+        nullDist[i]<-res$coefficients[2,3]
+      }
+      
+      pValueHigh<-2*(sum(nullDist >= testStat)+1)/(nperm+1)
+      pValueLow<-2*(sum(nullDist <= testStat)+1)/(nperm+1)
+      
+      pVal<-min(pValueHigh, pValueLow)
+    }
+  }
+  
+  if(method=="sign") 
+  {
+    icx<-ape::pic(x, tree)
+    icy<-ape::pic(y, tree)
+    xPos<-icx>0
+    yPos<-icy>0
+    testStat<-sum(xPos==yPos)
+    pValueLow<-2*pbinom(testStat, length(icx), prob=0.5)
+    pValueHigh<-2*pbinom(length(icx)-testStat, length(icx), prob=0.5)
+    pVal<-min(pValueHigh, pValueLow)
+  }
+  
+  if(method=="rank") 
+  {
+    icx<-ape::pic(x, tree)
+    icy<-ape::pic(y, tree)
+    dicx<-c(icx, -icx)	
+    dicy<-c(icy, -icy)
+    
+    rx<-rank(dicx, ties.method="average")
+    ry<-rank(dicy, ties.method="average")
+    res<-summary(lm(ry~rx))
+    testStat<-res$coefficients[2,1]
+    testStat <- cor(rx,ry)
+    nullDist<-numeric(nperm)
+    for(i in 1:nperm) 
+    {
+      picx<-sample(icx)
+      dpicx<-c(picx, -picx)
+      prx<-rank(dpicx, ties.method="average")
+      res<-summary(lm(ry~prx))
+      nullDist[i]<-res$coefficients[2,1]
+      #nullDist[i]<- cor(prx,ry)
+    }
+    
+    pValueHigh<-2*(sum(nullDist >= testStat)+1)/(nperm+1)
+    pValueLow<-2*(sum(nullDist <= testStat)+1)/(nperm+1)
+    
+    pVal<-min(pValueHigh, pValueLow)
+    
+  }
+  obj <- list(testStat=testStat, pVal=pVal)
+  class(obj) <- "picRegression"
+  obj
+}
+
 
 calculatePIC <- function(data,tree)
 {
